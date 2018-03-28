f11b55a3cafd4a4fd5d3e9c7eae6d5731a782084
win_lienming <728385437@qq.com>
2018-03-28 13:52:12 +0800

    update db

--- tickets_hibernate_sequence.sql
+++ tickets_hibernate_sequence.sql
@@ -2,9 +2,9 @@ CREATE TABLE tickets.hibernate_sequence
 (
     next_val bigint(20)
 );
-INSERT INTO tickets.hibernate_sequence (next_val) VALUES (38);
-INSERT INTO tickets.hibernate_sequence (next_val) VALUES (38);
-INSERT INTO tickets.hibernate_sequence (next_val) VALUES (38);
-INSERT INTO tickets.hibernate_sequence (next_val) VALUES (38);
-INSERT INTO tickets.hibernate_sequence (next_val) VALUES (38);
-INSERT INTO tickets.hibernate_sequence (next_val) VALUES (38);
+INSERT INTO tickets.hibernate_sequence (next_val) VALUES (72);
+INSERT INTO tickets.hibernate_sequence (next_val) VALUES (72);
+INSERT INTO tickets.hibernate_sequence (next_val) VALUES (72);
+INSERT INTO tickets.hibernate_sequence (next_val) VALUES (72);
+INSERT INTO tickets.hibernate_sequence (next_val) VALUES (72);
+INSERT INTO tickets.hibernate_sequence (next_val) VALUES (72);
