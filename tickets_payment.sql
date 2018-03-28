2f9fe5d9456afcfe8a59bca9b68d37b48b328513
win_lienming <728385437@qq.com>
2018-03-28 13:54:32 +0800

    update db

--- tickets_payment.sql
+++ tickets_payment.sql
@@ -1,2 +1,10 @@
+CREATE TABLE tickets.payment
+(
+    account_id int(11) PRIMARY KEY NOT NULL,
+    password varchar(20) NOT NULL,
+    balance double DEFAULT '0' NOT NULL,
+    user_id int(11),
+    is_site tinyint(1) DEFAULT '0' NOT NULL
+);
 INSERT INTO tickets.payment (account_id, password, balance, user_id, is_site) VALUES (1, '1234', 8000, 1, 0);
 INSERT INTO tickets.payment (account_id, password, balance, user_id, is_site) VALUES (2, '1234', 0, 2, 0);
